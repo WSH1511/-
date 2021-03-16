823b954ece96fdcad40ab0c375cdc41d7bcaae46
WSH1511 <79505455+WSH1511@users.noreply.github.com>
2021-03-16 23:07:10 +0800

    Add files via upload

---
+++ 210316/js/script.js
@@ -0,0 +1,33 @@
+var name =" hitman bang introduces hit it the 2nd audition";
+alert("hey yo" + name);
+
+var name= "kkkkkkk";
+var age= 333;
+var Second audition= true;
+var people= ["hit", "man", "bang"];
+
+alert(name);
+alert(age);
+alert(Second audition);
+alert(people[3]);
+
+function addition(num){
+	return num+10;
+}
+var age2= 27;
+var trueage2= addition(age2);
+alert(trueage2)
+
+var age3= 45;
+var trueage3= addition(age3);
+alert(trueage3);
+
+function test(){
+	a=333;
+	var b= 111;
+	if(a == 333) var c=444;
+}
+
+document.getElementsByTagName("p")[0].innerHTML ="hitman bang introduces hit it the 2nd audition";
+document.getElementsByTagName("p")[0].style.color = "red";
+document.getElementsByTagName("p")[0].style.fontSize = "30px";
